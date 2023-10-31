78aaa312ce41ca3cc2649ec8db9d71792774550b
Rian Wahid Prayogo <r14n4r1e5@gmail.com>
2023-10-31 14:03:20 +0700

    add some test

--- test.c
+++ test.c
@@ -1,6 +1,9 @@
 #include"gc.h"
 #include<assert.h>
+#include <stdio.h>
 #include<stdlib.h>
+#include<pthread.h>
+
 void *test_fn1(){
   void *ptr; 
   alloc_mem(&ptr,4);
@@ -24,7 +27,7 @@ void test_fn2(){
 void test(){
   test_fn2();
   void *a;
-  for(int i=0; i<100; i++){
+  for(int i=0; i<20; i++){
     {
       void *b;
       alloc_mem(&b,4);
@@ -40,7 +43,34 @@ void test(){
       assert(c==a);
     }
   }
-  force_free(a);
+  int total=200;
+  void ***ptrs=malloc(total*sizeof(void *));
+  void ***ptrs2=malloc(total*sizeof(void *));
+  void ***ptrs3=malloc(total*sizeof(void *));
+  for(int i=0; i<total; i++){
+    ptrs[i]=malloc(sizeof(void *));
+    ptrs2[i]=malloc(sizeof(void *));
+    ptrs3[i]=malloc(sizeof(void *));
+  }
+  for(int i=0; i<total; i++){
+    alloc_mem(ptrs[i],4);
+    *(int *)*ptrs[i]=i;
+    assign_ptr(ptrs2[i],*ptrs[i]);
+    assign_ptr(ptrs3[i],*ptrs2[i]);
+    *ptrs[i]=NULL;
+    clean();
+  }
+  for(int i=0; i<total; i++){
+    assert(*(int *)*ptrs2[i]==i);
+    assert(*(int *)*ptrs3[i]==i);
+    free(ptrs[i]);
+    free(ptrs2[i]);
+    free(ptrs3[i]);
+  }
+  free(ptrs);
+  free(ptrs2);
+  free(ptrs3);
+
 }
 
 int main(){
